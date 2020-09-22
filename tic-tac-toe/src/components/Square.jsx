45a67fde9160a922df1edff224f45d003c993dba
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-09-22 13:23:32 +0200

    updating handleClick function

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props); // why is super crossed out here?
-    this.state = { value: null };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
+const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
 
 export default Square;
