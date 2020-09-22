643d964ca393a5baabb5f8cd9392722e0119483d
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-09-22 12:50:30 +0200

    separating elements into different components and adding respective imports

---
+++ tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props); // why is super crossed out here?
+    this.state = { value: null };
+  }
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+export default Square;
--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,67 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Game from "./components/Game";
 import "./index.css";
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props); // why is super crossed out here?
-    this.state = { value: null };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
+const index = () => {
+  return (
+    <div>
+      <Game />
+    </div>
+  );
+};
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = "Next player: X";
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+export default index;
 
 // ========================================
 
